f6b59e1c72353d68cbba56ca6bbbbe34eac78254
渋谷 <shibuya@imagenious.co.jp>
2020-02-19 17:17:44 +0900

    refactor

--- src/Console/LanguageListCheckCommand.php
+++ src/Console/LanguageListCheckCommand.php
@@ -3,8 +3,11 @@
 namespace ShibuyaKosuke\LaravelLanguageSetting\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Lang;
 use Symfony\Component\Finder\SplFileInfo;
+use function Composer\Autoload\includeFile;
 
 class LanguageListCheckCommand extends Command
 {
@@ -23,22 +26,51 @@ class LanguageListCheckCommand extends Command
     protected $description = 'Show language list.';
 
     /**
+     * @param $language
+     * @return \Illuminate\Support\Collection
+     */
+    private function getFilenames($language)
+    {
+        return collect([
+            'json' => sprintf('%s%s%s.json', __DIR__, '/../Resources/lang/', $language),
+            'auth' => sprintf('%s%s%s/auth.php', __DIR__, '/../Resources/lang/', $language),
+            'pagination' => sprintf('%s%s%s/pagination.php', __DIR__, '/../Resources/lang/', $language),
+            'passwords' => sprintf('%s%s%s/passwords.php', __DIR__, '/../Resources/lang/', $language),
+            'validation' => sprintf('%s%s%s/validation.php', __DIR__, '/../Resources/lang/', $language),
+        ]);
+    }
+
+    private function getKeys($language)
+    {
+        $files = $this->getFilenames($language);
+        $files->map(function ($file, $key) {
+            if ($key === 'json') {
+                $keys = array_keys(json_decode(file_get_contents($file), true, 512, JSON_OBJECT_AS_ARRAY));
+            } else {
+            }
+        });
+    }
+
+    /**
      * @return void
      */
     public function handle(): void
     {
         $language = $this->argument('language');
+
         // file exist
-        $files = collect([
-            'json' => sprintf('%s.json', $language),
-            'auth' => sprintf('%s/auth.php', $language),
-            'pagination' => sprintf('%s/pagination.php', $language),
-            'passwords' => sprintf('%s/passwords.php', $language),
-            'validation' => sprintf('%s/validation.php', $language),
-        ]);
+        $files = $this->getFilenames($language);
+
         $res = $files->map(function ($file, $type) {
-            return file_exists(__DIR__ . '/../Resources/lang/' . $file) ? 'OK' : 'NG';
+            return file_exists($file) ? 'OK' : 'NG';
         });
+
         $this->table($res->keys()->toArray(), [$res->values()->toArray()]);
+
+        if ($res->contains('NG')) {
+            $this->error('Error: lack of files needed.');
+            return;
+        }
+        $this->getKeys($language);
     }
 }
